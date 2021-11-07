882da04b99e6d81e1f955acb7bc08e031d19d85c
Kimmeko Williams <actuary2022@gmail.com>
2021-11-07 14:08:52 -0500

    Exercise 3 Completed

---
+++ world.js
@@ -0,0 +1,24 @@
+onload= function(){
+    var btn=document.getElementById('lookup');
+    var request=new XMLHttpRequest();
+    var result=document.getElementById('result');
+    var country=document.getElementById('country');
+    btn.addEventListener('click',function(event){
+        event.preventDefault();
+        var url="world.php?country="+country.value;
+        request.onreadystatechange=hrequest;
+        request.open('GET',url,true);
+        request.send(); 
+    });
+
+    function hrequest(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if (request.status===200){
+                var response=request.responseText;
+                result.innerHTML=response;
+            }else{
+                result.innerHTML="Error. There was a problem with the request";
+            }
+        }
+    }
+} 
--- world.php
+++ world.php
@@ -1,11 +1,18 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+//$stmt = $conn->query("SELECT * FROM countries");
+if(isset($_GET['country'])){
+  $country = $_GET['country'];
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+}
+if(!isset($_GET['country'])){
+  $stmt = $conn->query("SELECT * FROM countries");
+}
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
