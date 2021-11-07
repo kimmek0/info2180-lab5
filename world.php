45b2ac954c521e73964dfaebec8b949af9a52810
Kimmeko Williams <actuary2022@gmail.com>
2021-11-07 14:51:02 -0500

    Exercise 4 Completed

--- world.php
+++ world.php
@@ -17,8 +17,23 @@ if(!isset($_GET['country'])){
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+  <thead>
+      <tr>
+          <th>Name</th>
+          <th>Continent</th>
+          <th>Idependence</th>
+          <th>Head of State</th>
+      </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($results as $row): ?>
+      <tr>
+          <td><?= $row['name']; ?></td>
+          <td><?= $row['continent']; ?></td>
+          <td><?= $row['independence_year']; ?></td>
+          <td><?= $row['head_of_state']; ?></td>
+      </tr>
+      <?php endforeach; ?>
+  </tbody>
+</table>
